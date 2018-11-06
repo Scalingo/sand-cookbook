b30f77648cc92b85977b43271a4111a025724091
Étienne Michon <etienne@scalingo.com>
2018-11-06 11:28:06 +0100

    Modify bin location

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,45 +9,40 @@
 
 require 'resolv'
 
-dirname = "sand-" +
-           node['sand']['version'] + "-linux-" +
-           node['sand']['arch']
-archive = "#{dirname}.tar.gz"
-
-download_url =
-  node['sand']['download_url'] + "/" +
-  node['sand']['version'] + "/" + archive
-
-archive_dest_path = "#{Chef::Config[:file_cache_path]}/#{archive}"
+dirname = "sand-#{node['sand']['version']}-linux-#{node['sand']['arch']}"
+download_url = "#{node['sand']['download_url']}/#{node['sand']['version']}"
 dir_dest_path = "#{Chef::Config[:file_cache_path]}/#{dirname}"
 
-remote_file archive_dest_path do
-  source download_url
+{
+  'sand-agent_linux_amd64' => 'sand-agent',
+  'sand-agent-cli_linux_amd64' => 'sand-cli',
+}.each do |bin_name, dest_bin_name|
+  remote_file dir_dest_path do
+    source "#{download_url}/#{bin_name}"
+  end
+
+  bash "Install #{bin_name} #{node['sand']['version'] }" do
+    code <<-EOH
+      cp #{dir_dest_path}/#{bin_name} #{node['sand']['install_path']}/#{dest_bin_name}
+    EOH
+    subscribes :run, "remote_file[#{dir_dest_path}]"
+    action :nothing
+  end
 end
 
-bash "extract sand #{node['sand']['version'] }" do
-  code <<-EOH
-    tar -C #{Chef::Config[:file_cache_path]} -xvf #{archive_dest_path}
-    cp #{dir_dest_path}/sand-agent #{node['sand']['install_path']}
-    cp #{dir_dest_path}/sand-agent-cli #{node['sand']['install_path']}
-  EOH
-  subscribes :run, "remote_file[#{archive_dest_path}]"
-  action :nothing
-end
+binary_path = ::File.join(node['sand']['install_path'], 'sand-agent')
 
-binary_path = File.join(node['sand']['install_path'], "sand-agent")
-
-env = node['sand']['env'].select{|k, v|
+env = node['sand']['env'].select do |k, v|
   !v.nil? && v && v != ""
-}.map{|k, v|
+end.map do |k, v|
   "#{k.upcase}=#{v}"
-}
+end
 
 manager = Chef::Provider::Service::Upstart
-if node['init_package'] == "systemd"
+if node['init_package'] == 'systemd'
   manager = Chef::Provider::Service::Systemd
 
-  systemd_unit "sand-agent.service" do
+  systemd_unit 'sand-agent.service' do
     action :create
     systemd_content = {
       "Unit" => {
@@ -78,7 +73,7 @@ end
 
 service 'sand-agent' do
   provider manager
-  subscribes :restart, "remote_file[#{archive_dest_path}]"
+  subscribes :restart, "remote_file[#{dir_dest_path}]"
   action [:enable, :start]
 end
 
