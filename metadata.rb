2500321260c4bd86328deb425d3783c2f9f48749
Soulou <leo@unbekandt.eu>
2019-05-01 00:09:19 +0200

    Bump v0.0.3

--- metadata.rb
+++ metadata.rb
@@ -8,4 +8,4 @@ chef_version     '>= 12'
 supports         'ubuntu'
 description      'Install SAND'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
